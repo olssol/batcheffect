371955833129a53ec7dfc5eabb60e8e9eb894375
Chenguang Wang <chenguang.wang.0517@gmail.com>
2018-06-09 10:51:28 -0400

    add IR ratio model

--- batch_simu.R
+++ batch_simu.R
@@ -17,21 +17,31 @@ ALPHA     <- 0.05; ##two sided
 POWER     <- 0.8;
 
 ## response
-MU        <- 0;
+MU        <- c(0, 2);
+BETA      <- 0.3;
+
 PAR.ERROR <- list(gamma   = list(error.type = "normal", ysig = 0.132),
                   delta   = list(error.type = "normal", ysig = 0, mu = 1),
                   epsilon = list(error.type = "normal", ysig = 1));
 
+PAR.ERROR <- list(gamma   = list(error.type = "skewed", ysig = 0.132, skew.n = 2, skew.p = 0.5),
+                  delta   = list(error.type = "normal", ysig = 0, mu = 1),
+                  epsilon = list(error.type = "normal", ysig = 1));
+
+
+test <- simu.error(n.simu = 10000, error.type = "skewed", ysig = 0.132, skew.n = 2,   skew.p = 0.5);
+test <- simu.error(n.simu = 10000, error.type = "skewed", ysig = 0.132, skew.n = 1.5, skew.p = 0.1);
+##plot(density(test));
+
 ## conduct
-BATCH.SIZE <- NULL;
-N.BATCH    <- 1;
+BATCH.SIZE <- 37;
+N.BATCH    <- NULL;
 ## replications
 NREPS      <- 2;
 
 ##-----SIMULATION ---------
 rst <- simu.trial(p1 = P1, p0 = P0, batch.size = BATCH.SIZE, n.batch = N.BATCH,
-                  mu = MU, par.error = PAR.ERROR,
+                  mu = MU, beta = BETA,
+                  par.error = PAR.ERROR,
                   alpha = ALPHA, power = POWER,
                   nreps = NREPS, seed = 10000);
-rst
-
--- batch_tools.R
+++ batch_tools.R
@@ -22,20 +22,22 @@ sub.txt.w.no <- function(numbers, template.f, out.f="rst.txt", sub.str="AA") {
     write(tpla, file=out.f);
 }
 
+
 ##-----------------------------------------------------------------------------------
 ##-----------------------------------------------------------------------------------
 ##                       BATCH EFFECT
 ##-----------------------------------------------------------------------------------
 ##-----------------------------------------------------------------------------------
-get.truth <- function(mu, par.error, p0, p1, n.total = 100000) {
+get.truth <- function(mu, par.error, p0, p1, beta = 0, n.total = 100000) {
     true.pts <- simu.all.pts(batch.size = 1,
                              n.total = n.total,
                              mu = mu,
+                             beta = beta,
                              par.error = par.error);
     ## measure 1: ratio
     var.all     <- var(true.pts$Y);
     ## variance without batch effect
-    var.nobatch <- var(exp(mu + true.pts$epsilon));
+    var.nobatch <- var(true.pts$Y.NoBatch);
     var.ratio   <- var.all/var.nobatch - 1;
 
     ## cut off
@@ -71,7 +73,7 @@ simu.error <- function(n.simu,
         smu       <- skew.n * (1-skew.p) / skew.p;
         va        <- skew.n * (1-skew.p) / skew.p^2;
         noise.sig <- skew.noise;
-        rst       <- rnbinom(n.simu, skew.n, skew.p);
+        rst       <- rnbinom(n = n.simu, size = skew.n, prob = skew.p);
         rst       <- rst - smu + rnorm(n.simu, mu, noise.sig);
         rst       <- rst/sqrt(va + noise.sig^2)*ysig;
     });
@@ -79,14 +81,36 @@ simu.error <- function(n.simu,
     rst
 }
 
+simu.batch.error <- function(batch.size, n.batch,
+                             par.error  = list(gamma   = list(error.type = "normal", ysig = 1),
+                                               delta   = list(error.type = "normal", ysig = 1, mu = 1),
+                                               epsilon = list(error.type = "normal", ysig = 1))) {
+
+    beffs <- NULL;
+    for (ef in c("gamma", "delta")) {
+        cur.par <- par.error[[ef]];
+        cur.eff <- do.call(simu.error, c(n.simu = n.batch, cur.par));
+        beffs   <- cbind(beffs, cur.eff);
+    }
+
+    gamma   <- beffs[,1];
+    delta   <- beffs[,2];
+    epsilon <- do.call(simu.error,
+                       c(n.simu = batch.size * n.batch, par.error[["epsilon"]]));
+
+    error <- rep(gamma, each = batch.size) + rep(delta, each = batch.size) * epsilon;
+
+    list(gamma   = gamma,
+         delta   = delta,
+         epsilon = epsilon,
+         error   = error);
+}
+
+
 ## simulate for an individual batch
-simu.all.pts <- function(batch.size = 3,
-                         n.total    = batch.size,
-                         n.batch    = NULL,
-                         mu         = 0,
-                         par.error  = list(gamma   = list(error.type = "normal", ysig = 1),
-                                           delta   = list(error.type = "normal", ysig = 1, mu = 1),
-                                           epsilon = list(error.type = "normal", ysig = 1))) {
+simu.all.pts <- function(n.total, batch.size = 1, n.batch = NULL, mu = c(0, 0), beta = 0, ...) {
+
+    stopifnot(n.total > 0);
 
     if (!xor(is.null(batch.size), is.null(n.batch)))
         stop("Please specify either batch.size or n.batch.")
@@ -97,32 +121,49 @@ simu.all.pts <- function(batch.size = 3,
         n.batch <- as.numeric(ceiling(n.total / batch.size));
     }
 
-    cat("ntotal=", n.total, " n.batch=", n.batch, " batch.size=", batch.size, "\n");
+    nt             <- length(mu);
+    ystar          <- NULL;
+    ystar.nobatch  <- NULL;
+    gamma          <- NULL;
+    delta          <- NULL;
+    epsilon        <- NULL;
+    for (i in 1:nt) {
+        cur.mu <- mu[i];
+        if (i > 1)
+            cur.mu <- cur.mu + ystar.nobatch[, i-1];
 
-    beffs <- NULL;
-    for (ef in c("gamma", "delta")) {
-        cur.par <- par.error[[ef]];
-        cur.eff <- do.call(simu.error, c(n.simu = n.batch, cur.par));
-        beffs   <- cbind(beffs, cur.eff);
+        cur.error <- simu.batch.error(batch.size, n.batch, ...);
+
+        gamma    <- cbind(gamma,   cur.error$gamma);
+        delta    <- cbind(delta,   cur.error$delta);
+        epsiolon <- cbind(epsilon, cur.error$epsilon);
+
+        ystar         <- cbind(ystar,         cur.mu + cur.error$error);
+        ystar.nobatch <- cbind(ystar.nobatch, cur.mu + cur.error$epsilon);
     }
 
-    epsilon  <- do.call(simu.error,
-                        c(n.simu = n.total, par.error[["epsilon"]]));
+    if (1 == nt) {
+        ##absolute outcome
+        Y         <- exp(ystar);
+        Y.NoBatch <- exp(ystar.nobatch);
+    } else {
+        ## ratio
+        Y <- exp(ystar[,nt])/exp(ystar[,1]);
+        Y.NoBatch <- exp(ystar.nobatch[,nt])/exp(ystar.nobatch[,1]);
+    }
 
-    ystar <- rep(mu, n.total);
-    ystar <- ystar + rep(beffs[,1], each = batch.size)[1:n.total];
-    ystar <- ystar + rep(beffs[,2], each = batch.size)[1:n.total] * epsilon;
-    batch <- rep(1:n.batch, each = batch.size)[1:n.total];
-
-    list(Y          = exp(ystar),
-         batch      = batch,
-         Ystar      = ystar,
+    list(Y          = Y[1:n.total],
+         Y.NoBatch  = Y.NoBatch[1:n.total],
+         Ystar      = ystar[1:n.total,],
+         Ystar.nob  = ystar.nobatch[1:n.total,],
          mu         = mu,
+         beta       = beta,
+         batch      = rep(1:n.batch, each = batch.size)[1:n.total],
          batch.size = batch.size,
-         gamma      = beffs[,1],
-         delta      = beffs[,2],
-         epsilon    = epsilon,
-         par.error  = par.error);
+         n.batch    = n.batch,
+         gamma      = gamma,
+         delta      = delta,
+         epsilon    = epsilon);
 }
 
 
@@ -131,16 +172,17 @@ simu.trial <- function(p1 = 0.3, p0 = 0.15,
                        alpha = 0.05, power = 0.8,
                        batch.size = 3, n.batch = NULL,
                        nreps = 10000,
-                       mu = 0, par.error = list(gamma   = list(error.type = "normal", ysig = 1),
-                                                delta   = list(error.type = "normal", ysig = 1, mu = 1),
-                                                epsilon = list(error.type = "normal", ysig = 1)),
+                       mu = 0, beta = 0,
+                       par.error = list(gamma   = list(error.type = "normal", ysig = 1),
+                                        delta   = list(error.type = "normal", ysig = 1, mu = 1),
+                                        epsilon = list(error.type = "normal", ysig = 1)),
                        cuty.h01 = NULL, n.truepts = 1000000, seed = NULL) {
 
     if (is.numeric(seed))
         set.seed(seed);
 
     ## truth
-    truth <- get.truth(mu, par.error, p0, p1, n.total = n.truepts);
+    truth <- get.truth(mu, par.error, p0, p1, beta = beta, n.total = n.truepts);
 
     if (is.null(cuty.h01))
         cuty.h01 <- truth[c("cuty.h0", "cuty.h1")];
@@ -152,7 +194,6 @@ simu.trial <- function(p1 = 0.3, p0 = 0.15,
     ## replications
     all.rst <- NULL;
     for (rep in 1:NREPS) {
-        print(rep);
         cur.rst <- NULL;
 
         ##---- simon design ------------
@@ -164,6 +205,7 @@ simu.trial <- function(p1 = 0.3, p0 = 0.15,
                                    batch.size = batch.size,
                                    n.batch    = n.batch,
                                    mu         = mu,
+                                   beta       = beta,
                                    par.error  = par.error);
             cur.simon.pt <- c(cur.simon.pt, cur.pt$Y);
         }
@@ -178,7 +220,10 @@ simu.trial <- function(p1 = 0.3, p0 = 0.15,
                                       batch.size = batch.size,
                                       n.batch    = n.batch,
                                       mu         = mu,
+                                      beta       = beta,
                                       par.error  = par.error);
+
+
         for (cuty in cuty.h01) {
             cur.rst <- c(cur.rst,
                          sum.single(cur.single.pt$Y > cuty, alpha, p0));
@@ -329,4 +374,83 @@ sum.simon.all <- function(simon.rst, truep) {
       RejRate = all.mean[2],
       Bias = bias,
       MSE = mse);
+}
+
+##-----------------------------------------------------------------------------------
+##-----------------------------------------------------------------------------------
+##                       SIMULATION RESULTS
+##-----------------------------------------------------------------------------------
+##-----------------------------------------------------------------------------------
+simu.combine <- function(path    = "./Results/absoutcome/",
+                         sce     = 1:4,
+                         batch   = c(paste("batchsize", c(1,3,6,9), sep=""), "nbatch2", "nbatch1"),
+                         vratio  = c("0.05", "0.10", "0.15", "0.20"),
+                         postfix = c("", "_gamma_fixed", "_gamma_skewed")) {
+    overall.rst <- NULL;
+    for (s in sce) {
+        for (bs in batch) {
+            for (v in vratio) {
+                for (pf in postfix) {
+                    ## load data
+                    cur.f <- paste(path, "simu_sce", s, "_", bs, "_vratio", v, pf, ".Rdata", sep="");
+                    load(cur.f);
+
+                    if (!is.null(rst$batch.size)) {
+                        bsize <- rst$batch.size;
+                    } else if (2 == rst$n.batch) {
+                        bsize <- "B2";
+                    } else {
+                        bsize <- "BF";
+                    }
+
+                    ## organize results
+                    cur.rst     <- data.frame(VRatio = v,
+                                              Sce    = s,
+                                              Study  = pf,
+                                              p0     = rst$p01[1],
+                                              p1     = rst$p01[2],
+                                              Batch  = bsize,
+                                              MSize  = as.numeric(rst$design.simon["n"]),
+                                              MExp   = as.numeric(rst$design.simon["EN(p0)"]),
+                                              M0N    = as.numeric(rst$sum.simon.h0["AvgN"]),
+                                              M0Rej  = as.numeric(rst$sum.simon.h0["RejRate"]),
+                                              M1N    = as.numeric(rst$sum.simon.h1["AvgN"]),
+                                              M1Rej  = as.numeric(rst$sum.simon.h1["RejRate"]),
+                                              S0CIW  = as.numeric(rst$sum.single.h0["AvgCIW"]),
+                                              S0Rej  = as.numeric(rst$sum.single.h0["RejRate"]),
+                                              S0MSE  = as.numeric(rst$sum.single.h0["MSE"]),
+                                              S0Bias = as.numeric(rst$sum.single.h0["Bias"]),
+                                              S1CIW  = as.numeric(rst$sum.single.h1["AvgCIW"]),
+                                              S1Rej  = as.numeric(rst$sum.single.h1["RejRate"]),
+                                              S1MSE  = as.numeric(rst$sum.single.h1["MSE"]),
+                                              S1Bias = as.numeric(rst$sum.single.h1["Bias"]));
+
+                    ## append
+                    overall.rst <- rbind(overall.rst, cur.rst);
+                }
+            }
+        }
+    }
+
+    row.names(overall.rst) <- NULL;
+    data.frame(overall.rst);
+}
+
+plot.simu <- function(rst.data, y, ylab, yinter = NULL,  ylim = NULL) {
+
+    rst <- ggplot(data = rst.data, aes(x = Batch, group = VRatio)) +
+        geom_line(aes_string(y = y, col = "VRatio")) +
+        theme_bw() +
+        labs(y = ylab, x = "Batch Size") +
+        theme(legend.position = "top") +
+        facet_grid(Group~Sce);
+
+    if (!is.null(ylim))
+        rst <- rst + scale_y_continuous(limits = ylim);
+
+    if (!is.null(yinter))
+        rst <- rst + geom_hline(yintercept = yinter, linetype="dashed", col = "red");
+
+
+    rst
 }
